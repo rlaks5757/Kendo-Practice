cb5d5624d97f03cefd991aa402832114f472e665
rlaks5757 <rlaks5757@gmail.com>
2022-08-11 10:40:12 +0900

    save

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import TileLayoutMain from "./TileLayout/TileLayout";
 import CWP from "./TestTable/CWP";
+import Milestone2 from "./TestTable/MileStone";
+import ActStatus from "./TestTable/ActStatus";
 
 const Router = () => {
   return (
@@ -9,6 +11,8 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<TileLayoutMain />} />
         <Route path="/:project_code" element={<CWP />} />
+        <Route path="/:project_code/act" element={<ActStatus />} />
+        <Route path="/:project_code/mile" element={<Milestone2 />} />
       </Routes>
     </BrowserRouter>
   );
